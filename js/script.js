776e6d5b11b0c7ee4ac5d058a6390e365e7752cb
yoanlcq <yoanlecoq.io@gmail.com>
2016-11-04 12:42:52 +0100

    Ajouté les pastilles de navigation, et un brouillon du code pour les animer.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-function rien() {}
+function glisser(elid) {
+    $('html, body').animate({
+        scrollTop: $("#"+elid).offset().top
+    }, 400);
+}
+
+var pastilles = [];
+var posters = [];
+var anciennes_largeurs = [];
+function pastilles_capturer() {
+    posters   = document.getElementsByClassName("poster");
+    pastilles = document.getElementsByClassName("pastille");
+    for(var i=0 ; i<pastilles.length ; ++i)
+        anciennes_largeurs[i] = pastilles[i].scrollWidth;
+}
+function pastilles_frame(timestamp) {
+    for(var i=0 ; i<pastilles.length ; ++i) {
+        var dist  = Math.abs(posters[i].top - window.scrollTop);
+        var poids = Math.max(0, 1 - dist/posters[i].scrollHeight);
+        pastilles[i].style.width = anciennes_largeurs * poids;
+    }
+}
+function pastilles_chargees() {
+    pastilles_capturer();
+    window.requestAnimationFrame(pastilles_frame);
+}
+// Commenté, parce que ça ne marchera pas tout de suite.
+//$(function(){pastilles_chargees();}) //quand le DOM est pret...
